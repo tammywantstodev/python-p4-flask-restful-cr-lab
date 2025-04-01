1bf98421e462f142b5ace8285d7ae15ce44fe8ea
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-04-01 12:56:05 +0300

    saving changes

--- server/app.py
+++ server/app.py
@@ -17,10 +17,45 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list=[p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+
+        return response
+    def post(self):
+        data = request.get_json()
+        new_plant=Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(), 201)
+
+
+    
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        if plant is None:
+            return make_response({"error": "Plant not found"}, 404)
+          
+        response = make_response(
+            plant,
+            200,
+        )
+
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')
+
         
 
 if __name__ == '__main__':
